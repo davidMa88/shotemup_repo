2d31e194f1418c8bebab1544c07a4bc0cdd05fc1
davidMa <david.mae20@gmail.com>
2016-02-07 19:19:38 +0100

    Project set up

---
+++ shootemup/Assets/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float health;
+	public float velocidad;
+
+	public GameObject bullet;
+	GameObject newBullet;
+
+	void Start(){
+	}
+
+	void Update(){
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+			newBullet = (GameObject)Instantiate (bullet, transform.position, Quaternion.identity);
+			Debug.Log (newBullet.transform.position);
+		}
+
+		if (Input.GetKey (KeyCode.X)) {
+			Destroy(newBullet);
+		}
+
+		if (newBullet != null) {
+			newBullet.transform.position += Vector3.down * Time.deltaTime * velocidad;
+		}
+	}
+
+}
---
+++ shootemup/Assets/PayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class PayerMovement : MonoBehaviour {
+
+	public float maxSpeed;
+	public float aceleration;
+	[Range(0f, 1f)]
+	public float brake;
+
+	public Text speedDebug;
+
+	Rigidbody2D rigidBody;
+	LineRenderer axisDebug;
+	Vector3 axis;
+
+	float normalizedBrake;
+	float minBrakeValue = 0.5f;
+
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+		axisDebug = GetComponent<LineRenderer> ();
+	}
+
+	void Update () {
+		
+		float dx = Input.GetAxis("Horizontal");
+		float dy = Input.GetAxis("Vertical");
+
+		rigidBody.velocity += new Vector2 (dx, dy) * Time.fixedDeltaTime * aceleration;
+
+		if (Input.GetKey (KeyCode.Space)) {
+			rigidBody.velocity *= normalizedBrake;
+			axis *= normalizedBrake;
+		}
+		else if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.RightArrow)) {
+			axis = new Vector3 (Input.GetAxis ("Horizontal") * 2, Input.GetAxis ("Vertical") * 2, 0);
+		}
+
+		speedDebug.text = rigidBody.velocity.magnitude.ToString ();
+		axisDebug.SetPosition (0, transform.position);
+		axisDebug.SetPosition (1, transform.position + axis);
+	}
+
+	void FixedUpdate() {
+		
+		if(rigidBody.velocity.magnitude > maxSpeed)
+		{
+			rigidBody.velocity = rigidBody.velocity.normalized * maxSpeed;
+		}
+
+	}
+
+	void OnValidate(){
+
+		normalizedBrake = brake;
+		normalizedBrake = minBrakeValue + normalizedBrake - (minBrakeValue * normalizedBrake);  // <-- Obligo que sea entre 0.5f y 1f 
+		normalizedBrake = minBrakeValue - normalizedBrake + 1.0f;						 		// <-- Invierto el valoe (cuando 0.5f es 1f, cuando 1f es 0.5f)
+
+		Debug.Log(normalizedBrake);
+	}
+
+}
